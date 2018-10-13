f28f00a1e0a5c9d7feced040d748cab5a0e2f7d7
Vlad Sorokin <malchugan666@gmail.com>
2018-10-13 07:08:47 +0300

    initial commit

--- capsule_network.py
+++ capsule_network.py
@@ -206,7 +206,7 @@ if __name__ == "__main__":
 
     print("# parameters:", sum(param.numel() for param in model.parameters()))
 
-    optimizer = Adam(model.parameters(),lr=1e-4)
+    optimizer = Adam(model.parameters(),lr=5e-6)
 
     capsule_loss = CapsuleLoss()
 
